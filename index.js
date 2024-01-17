c0e669233d600db5db77386bc61d8dd4501e1848
JulianOjea <32637786+JulianOjea@users.noreply.github.com>
2024-01-17 12:01:33 +0100

    Add finish server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-//Paht público
+//Path público
 const publicPath = path.resolve(__dirname, 'public'); //apunta a donde está montado el servidor
 app.use(express.static(publicPath));
 
@@ -19,5 +19,5 @@ app.use(express.static(publicPath));
 server.listen(process.env.PORT, (err) => { //callback que develve un error si sucede
     if (err) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto!!', process.env.PORT); //si no hay ningún error se loguea esto
+    console.log('Servidor corriendo en puerto ey :D!!', process.env.PORT); //si no hay ningún error se loguea esto
 })
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,24 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+
+const Bands = require('../models/bands')
+
+const bands = new Bands();
+
+bands.addBand( new Band('Istasha'));
+bands.addBand( new Band('Troikadedra'));
+bands.addBand( new Band('Children of Bodom'));
+bands.addBand( new Band('Kamaara'));
+
+console.log(bands)
 
 //Mensajes de sockets
 io.on('connection', client => { // client: cliente que se acaba de conectar al socket server
     console.log('Cliente conectado');
     
+    //envia mensaje al cliente que se acaba de conectar
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     }); //se lanzará cuando el cliente se desconecte
@@ -12,6 +27,30 @@ io.on('connection', client => { // client: cliente que se acaba de conectar al s
         console.log('esto es un mensajee!', payload);
 
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    })
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     //io.emit('nuevo-mensaje', payload); //emite a todos
+    //     //client.broadcast.emit('nuevo-mensaje', {'nombre': payload['nombre'], 'mensaje': payload['mensaje']}); // emite a todos menos al que lo emitió
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // })
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        //io es el servidor y manda a todos los servidores
+        io.emit('active-bands', bands.getBands());
+    })
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    })
+
+    //delete-band
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     })
 });
 
